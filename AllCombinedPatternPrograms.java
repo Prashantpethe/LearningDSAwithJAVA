5887ad32fc91963009658f0b296ba18ae386f280
Prashantpethe <prashantpethe7@gmail.com>
2024-04-28 20:04:03 +0530

    combined pattern programs

---
+++ AllCombinedPatternPrograms.java
@@ -0,0 +1,62 @@
+public class AllCombinedPatternPrograms {
+
+        static void print1(int n){
+//output
+//* * * * *
+//* * * * *
+//* * * * *
+// * * * * *
+// * * * * *
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+
+        }
+        static void Nby2Forest(int n)
+        {
+//Output
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<=i;j++)
+                {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+        static void nTriangles(int n){
+//output
+//        1
+//        12
+//        123
+//        1234
+//        12345
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+        }
+        public static void main(String[] args) {
+            int n=5;
+            print1(n);
+            System.out.println();
+            Nby2Forest(n);
+            System.out.println();
+            nTriangles(n);
+            System.out.println();
+            //    rightAngledNumberPyramid(n);
+
+        }
+    }
---
+++ out/production/LearningDSAwithJAVA/README.md
@@ -0,0 +1,4 @@
+# LearningDSAwithJAVA
+DSA programs from basic to advance
+
+This repository is prepared to maintain allpractice programs from basic to advance including DSA in java language
