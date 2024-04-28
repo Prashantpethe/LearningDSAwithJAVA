58911c06179087fdbd9e08002fb2e0e8cc5bb262
Prashantpethe <prashantpethe7@gmail.com>
2024-04-28 20:47:32 +0530

    added rightAngledNumberPyramid

--- AllCombinedPatternPrograms.java
+++ AllCombinedPatternPrograms.java
@@ -48,6 +48,15 @@ public class AllCombinedPatternPrograms {
                 System.out.println();
             }
         }
+        static void rightAngledNumberPyramid(int n){
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=i;j++){
+                    System.out.print(i+"");
+                }
+                System.out.println();
+            }
+        }
         public static void main(String[] args) {
             int n=5;
             print1(n);
@@ -56,7 +65,7 @@ public class AllCombinedPatternPrograms {
             System.out.println();
             nTriangles(n);
             System.out.println();
-            //    rightAngledNumberPyramid(n);
+            rightAngledNumberPyramid(n);
 
         }
     }
